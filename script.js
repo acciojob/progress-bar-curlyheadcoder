b599b88944ae71c10a97b31dc03484f8869703e7
curlyheadcoder <mayanksonwani078@gmail.com>
2025-06-05 19:09:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,40 @@
-//your JS code here. If required.
 const progress = document.getElementById('progress');
-const prevBtn = document.getElementById('prev');
-const nextBtn = document.getElementById('next');
-const circles = document.getElementById('.circle');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+const circles = document.querySelectorAll('.circle');
 
 let currentActive = 1;
-nextBtn.addEventListener('click',() =>{
-	currentActive++;
-	if(currentActive > circles.length){
-		currentActive = circles.length;
-	}
-	update();
+
+next.addEventListener('click', () => {
+  currentActive++;
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+  update();
 });
-prevBtn.addEventListener('click', () => {
-	currentActive--;
-	if(currentActive < 1){
-		currentActive = 1;
-	}
-	update();
+
+prev.addEventListener('click', () => {
+  currentActive--;
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+  update();
 });
+
 function update() {
-	circles.forEach((circle, idx) => {
-		if(idx < currentActive){
-			circle.classList.add('active');
-		}else{
-			circle.classList.remove('active');
-		}
-	});
-	const progressPercent = ((currentActive-1) / (circles.length-1))*100;
-	progress.style.width = `${progressPercent}%`;
-	prevBtn.disabled = currentActive === 1;
-	nextBtn.disabled = currentActive === circles.length;
+  circles.forEach((circle, idx) => {
+    if (idx < currentActive) {
+      circle.classList.add('active');
+    } else {
+      circle.classList.remove('active');
+    }
+  });
+
+  const activeCircles = document.querySelectorAll('.circle.active').length;
+  const total = circles.length;
+  const progressPercent = ((activeCircles - 1) / (total - 1)) * 100;
+  progress.style.width = `${progressPercent}%`;
+
+  prev.disabled = currentActive === 1;
+  next.disabled = currentActive === circles.length;
 }
